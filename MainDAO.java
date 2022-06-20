7d0f1712614a70d77a62c898d3d02668647d30c7
ujjwal856 <92670365+ujjwal856@users.noreply.github.com>
2022-06-20 13:22:32 +0530

    Create MainDAO.java
    This is the DAO java file where all the queries are implemented.

---
+++ MainDAO.java
@@ -0,0 +1,33 @@
+package com.example.mnotes.Database;
+
+
+import android.icu.text.Replaceable;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
+
+import com.example.mnotes.Models.Notes;
+
+import java.util.List;
+
+@Dao
+public interface MainDAO {
+    @Insert(onConflict = REPLACE)
+    void insert(Notes notes);
+
+    @Query("SELECT*FROM notes ORDER BY id DESC")
+    List<Notes> getAll();
+
+    @Query("UPDATE notes SET title = :title, notes = :notes WHERE id = :id")
+    void update(int id,String title,String notes);
+
+    @Delete
+    void delete(Notes notes);
+
+    @Query("UPDATE notes SET pinned = :pin WHERE ID = :id")
+    void pin(int id,boolean pin);
+}
